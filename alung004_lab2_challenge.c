3364ebbeb3daf755f05edaa6e5fe2d341c6f1541
Ubuntu <alung004@ucr.edu>
2021-01-12 17:50:41 -0800

    first commit

---
+++ alung004_lab2_challenge.c
@@ -0,0 +1,51 @@
+/*	Author: lab
+ *  Partner(s) Name: Andrew Lung
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif	
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; 
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0x00; PORTC = 0xFF;
+	DDRD = 0xFF; PORTD = 0x00; 
+	unsigned char tmpA;
+	unsigned char tmpB;
+	unsigned char tmpC;
+	unsigned char tmpD = 0x00;
+	unsigned char totWeight = 0x00; 
+	while(1) {
+		tmpA = PINA;
+		tmpB = PINB;
+		tmpC = PINC;
+		tmpD = 0;
+		totWeight = tmpA + tmpB + tmpC;
+		if (totWeight > 140) {
+			tmpD = tmpD | 0x01;
+		}
+		//avoid underflow
+		if (tmpC > tmpA) {
+			if ((tmpC - tmpA) > 80) {
+				tmpD = tmpD | 0x02;
+			}
+		}
+		else if (tmpA > tmpC) {
+			if ((tmpA - tmpC) > 80) {
+				tmpD = tmpD | 0x02;
+			}		
+		}
+		totWeight = totWeight & 0xFC;
+		tmpD = tmpD | totWeight;
+		PORTD = tmpD;
+	}
+	return 0;
+}
+
---
+++ alung004_lab2_part3.c
@@ -0,0 +1,44 @@
+/*	Author: lab
+ *  Partner(s) Name: Andrew Lung
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif	
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; 
+	DDRC = 0xFF; PORTC = 0x00; 
+	unsigned char tmpA = 0x00; 
+	unsigned char cntavail = 0x00;
+while(1) {
+		cntavail = 0;
+		tmpA = PINA;
+		if (tmpA & 0x01) {
+			cntavail++;
+		}
+		if (tmpA & 0x02) {
+			cntavail++;
+		}
+		if (tmpA & 0x04) {
+			cntavail++;
+		}
+		if (tmpA & 0x08) {
+			cntavail++;
+		}
+		PORTC = ((tmpA & 0x70) | cntavail);
+///*
+		if (cntavail == 4) {
+			PORTC = PORTC | 0x80;
+		}
+//*/
+	}
+	return 0;
+}
+
